10a1435c2416af835765148badc1dc46b7c50501
Rytis Grincevičius <rytis.grincevicius@gmail.com>
2022-05-01 09:55:18 +0300

    feat: track gubbins transfer events

---
+++ src/datasources/gubbins.ts
@@ -0,0 +1,87 @@
+import {
+	GubbinsTransfer,
+} from '../../generated/schema'
+
+import {
+	Transfer as TransferEvent,
+	// Approval as ApprovalEvent,
+} from '../../generated/Gubbins/Gubbins'
+
+import {
+	constants,
+	decimals,
+	events,
+	transactions,
+} from '@amxx/graphprotocol-utils'
+
+import {
+	fetchAccount,
+} from '../fetch/account'
+
+import { fetchGubbins, fetchGubbinsBalance } from '../fetch/gubbins'
+
+export function handleTransfer(event: TransferEvent): void {
+	let contract   = fetchGubbins(event.address)
+	let ev         = new GubbinsTransfer(events.id(event))
+	ev.emitter     = contract.id
+	ev.contract    = contract.id
+	ev.transaction = transactions.log(event).id
+	ev.timestamp   = event.block.timestamp
+	ev.value       = decimals.toDecimals(event.params.value, contract.decimals)
+	ev.valueExact  = event.params.value
+
+	if (event.params.from.toHex() == constants.ADDRESS_ZERO) {
+		let totalSupply        = fetchGubbinsBalance(contract, null)
+		totalSupply.valueExact = totalSupply.valueExact.plus(event.params.value)
+		totalSupply.value      = decimals.toDecimals(totalSupply.valueExact, contract.decimals)
+		totalSupply.save()
+	} else {
+		let from               = fetchAccount(event.params.from)
+		let balance            = fetchGubbinsBalance(contract, from)
+		balance.valueExact     = balance.valueExact.minus(event.params.value)
+		balance.value          = decimals.toDecimals(balance.valueExact, contract.decimals)
+		balance.save()
+
+		ev.from                = from.id
+		ev.fromBalance         = balance.id
+	}
+
+	if (event.params.to.toHex() == constants.ADDRESS_ZERO) {
+		let totalSupply        = fetchGubbinsBalance(contract, null)
+		totalSupply.valueExact = totalSupply.valueExact.minus(event.params.value)
+		totalSupply.value      = decimals.toDecimals(totalSupply.valueExact, contract.decimals)
+		totalSupply.save()
+	} else {
+		let to                 = fetchAccount(event.params.to)
+		let balance            = fetchGubbinsBalance(contract, to)
+		balance.valueExact     = balance.valueExact.plus(event.params.value)
+		balance.value          = decimals.toDecimals(balance.valueExact, contract.decimals)
+		balance.save()
+
+		ev.to                  = to.id
+		ev.toBalance           = balance.id
+	}
+	ev.save()
+}
+
+// export function handleApproval(event: ApprovalEvent): void {
+// 	let contract = fetchERC20(event.address)
+
+// 	let owner           = fetchAccount(event.params.owner)
+// 	let spender         = fetchAccount(event.params.spender)
+// 	let approval        = fetchERC20Approval(contract, owner, spender)
+// 	approval.valueExact = event.params.value
+// 	approval.value      = decimals.toDecimals(event.params.value, contract.decimals)
+// 	approval.save()
+
+// 	// let ev         = new ERC20ApprovalEvent(events.id(event))
+// 	// ev.emitter     = contract.id
+// 	// ev.transaction = transactions.log(event).id
+// 	// ev.timestamp   = event.block.timestamp
+// 	// ev.token       = token.id
+// 	// ev.owner       = owner.id
+// 	// ev.spender     = spender.id
+// 	// ev.approval    = approval.id
+// 	// ev.value       = value.value
+// 	// ev.save()
+// }
